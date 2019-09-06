7e349c3ad994755a0130fa990839fdbd2948a2a7
Paul Melnikow <email@paulmelnikow.com>
2019-09-06 10:33:12 -0400

    Add more

--- dev.py
+++ dev.py
@@ -31,6 +31,23 @@ def test():
 
 
 @cli.command()
+def coverage():
+    execute("pytest --cov=harrison")
+
+
+@cli.command()
+def coverage_report():
+    execute("coverage html")
+    execute("open htmlcov/index.html")
+
+
+@cli.command()
+def test_both():
+    execute("python2 -m pytest")
+    execute("python3 -m pytest")
+
+
+@cli.command()
 def lint():
     execute("flake8", *python_source_files())
 
@@ -43,6 +60,13 @@ def black():
 @cli.command()
 def black_check():
     execute("black", "--check", *python_source_files())
+
+
+@cli.command()
+def publish():
+    execute("rm -rf dist/")
+    execute("python setup.py sdist")
+    execute("twine upload dist/*")
 
 
 if __name__ == "__main__":
